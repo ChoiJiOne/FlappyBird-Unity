4a7135ab02a8acea57b63e9bf0a1d17c3c80055d
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-22 16:45:01 +0900

    멤버 변수로 트랜스폼과 스프라이트 렌더러 추가

--- Assets/Script/Background.cs
+++ Assets/Script/Background.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Background : MonoBehaviour
 {
     /// <summary>
+    /// 백드라운드의 트랜스폼입니다. 백그라운드의 스케일 값을 조정하기 위한 멤버 변수입니다.
+    /// </summary>
+    private Transform _transform;
+
+    /// <summary>
+    /// 백그라운드가 렌더링할 스프라이트를 설정할 스프라이트 렌더러입니다.
+    /// </summary>
+    private SpriteRenderer _spriteRenderer;
+        
+    /// <summary>
     /// 첫 프레임이 시작되기 전에 호출합니다.
     /// </summary>
     void Start()
     {
+        _transform = GetComponent<Transform>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     /// <summary>
