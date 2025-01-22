29f96f8a702c72ec078826d0ce64fbc695d7ef9f
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-22 20:59:48 +0900

    타이틀의 움직임을 제어하는 스크립트 추가

---
+++ Assets/Scripts/TitleMoveController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 시작 씬의 타이틀을 움직입니다.
+/// </summary>
+public class TitleMoveController : MonoBehaviour
+{
+    /// <summary>
+    /// 첫 프레임이 시작되기 전에 호출합니다.
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// 매 프레임 한 번 호출합니다.
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
