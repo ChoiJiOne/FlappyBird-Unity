1a72a0779942e445f68dea4fd2fa0b958312a9cb
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-22 22:19:41 +0900

    타이틀이 움직이는 시간도 제어할 수 있도록 추가

--- Assets/Scripts/Editor/TitleMoveControllerEditor.cs
+++ Assets/Scripts/Editor/TitleMoveControllerEditor.cs
@@ -16,12 +16,17 @@ public class TitleMoveControllerEditor : Editor
     /// <remarks>
     /// 참조: https://docs.unity3d.com/6000.0/Documentation/ScriptReference/SerializedProperty.html
     /// </remarks>
-
     private SerializedProperty _moveHeight;
+
+    /// <summary>
+    /// 에디터에서 타이틀의 이동 시간 값입니다.
+    /// </summary>
+    private SerializedProperty _moveTime;
 
     void OnEnable()
     {
         _moveHeight = serializedObject.FindProperty("_moveHeight");
+        _moveTime = serializedObject.FindProperty("_moveTime");
     }
 
     /// <summary>
@@ -31,6 +36,7 @@ public class TitleMoveControllerEditor : Editor
     {
         serializedObject.Update();
         EditorGUILayout.PropertyField(_moveHeight);
+        EditorGUILayout.PropertyField(_moveTime);
         serializedObject.ApplyModifiedProperties();
     }
 }
