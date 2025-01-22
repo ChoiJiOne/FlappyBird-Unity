d875af7e199f66e4ba4c0d7c0671f55c9b97d76f
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-22 17:40:03 +0900

    배경이 낮인지 밤인지 확인하는 멤버 변수 추가

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -16,6 +16,11 @@ public class Background : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
 
     /// <summary>
+    /// 배경이 낮인지 밤인지 확인합니다. 낮이라면 true, 밤이라면 false입니다.
+    /// </summary>
+    private bool _isDay;
+
+    /// <summary>
     /// '낮' 백그라운드가 스프라이트입니다.
     /// </summary>
     private Sprite _dayBackground;
@@ -24,16 +29,26 @@ public class Background : MonoBehaviour
     /// '밤' 백그라운드 스프라이트입니다.
     /// </summary>
     private Sprite _nightBackground;
-    
+
     /// <summary>
     /// 첫 프레임이 시작되기 전에 호출합니다.
     /// </summary>
     void Start()
     {
+        _isDay = true;
         _dayBackground = Resources.Load<Sprite>("Sprite/background_day");
         _nightBackground = Resources.Load<Sprite>("Sprite/background_night");
         _transform = GetComponent<Transform>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (_isDay)
+        {
+            _spriteRenderer.sprite = _dayBackground;
+        }
+        else
+        {
+            _spriteRenderer.sprite = _nightBackground;
+        }
     }
 
     /// <summary>
@@ -41,6 +56,5 @@ public class Background : MonoBehaviour
     /// </summary>
     void Update()
     {
-        
     }
 }
