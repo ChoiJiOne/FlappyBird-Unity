925020193e9c907e412abd6c319e59ba446aa570
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-22 17:48:39 +0900

    백그라운드의 트랜스폼 멤버 변수 삭제

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -15,11 +15,6 @@ public class Background : MonoBehaviour
     }
 
     /// <summary>
-    /// 백드라운드의 트랜스폼입니다. 백그라운드의 스케일 값을 조정하기 위한 멤버 변수입니다.
-    /// </summary>
-    private Transform _transform;
-
-    /// <summary>
     /// 백그라운드가 렌더링할 스프라이트를 설정할 스프라이트 렌더러입니다.
     /// </summary>
     private SpriteRenderer _spriteRenderer;
@@ -47,7 +42,6 @@ public class Background : MonoBehaviour
         _isDay = true;
         _dayBackground = Resources.Load<Sprite>("Sprite/background_day");
         _nightBackground = Resources.Load<Sprite>("Sprite/background_night");
-        _transform = GetComponent<Transform>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
 
         if (_isDay)
