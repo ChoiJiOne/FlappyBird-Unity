5feed6fcc388b8b1664d46d3c966fcb500ce64da
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-23 08:28:18 +0900

    플레이 버튼을 클릭했을 때 선택 씬으로 전환

--- Assets/Scripts/Runtime/SceneSwitchManager.cs
+++ Assets/Scripts/Runtime/SceneSwitchManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// 시작 씬 내에서 버튼과 함께 씬 전환을 수행하는 매니저입니다.
@@ -8,10 +9,24 @@ using UnityEngine;
 public class SceneSwitchManager : MonoBehaviour
 {
     /// <summary>
+    /// 플레이 버튼을 클릭했을 때 전환할 씬의 이름입니다.
+    /// </summary>
+    private string _selectSceneName;
+
+    /// <summary>
+    /// 전환될 씬 이름을 설정합니다.
+    /// </summary>
+    void Start()
+    {
+        _selectSceneName = "SelectScene";
+    }
+
+    /// <summary>
     /// 플레이 버튼을 클릭했을 때 수행할 동작입니다.
     /// </summary>
     public void OnClickPlay()
     {
+        SceneManager.LoadScene(_selectSceneName);
     }
 
     /// <summary>
