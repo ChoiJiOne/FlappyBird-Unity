59ca5a181834fb9f05e5139b1adc16f69e719066
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-23 08:04:41 +0900

    씬 전환을 수행하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/SceneSwitchManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 버튼과 함께 씬 전환을 수행하는 매니저입니다.
+/// </summary>
+public class SceneSwitchManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 시작 씬(StartScene)에서 플레이 버튼을 클릭했을 때 수행할 동작입니다.
+    /// </summary>
+    public void OnClickPlay()
+    {
+
+    }
+}
