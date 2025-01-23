61aeaebceac13979b9278ed0aadf9b159571ea44
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-23 09:41:45 +0900

    백그라운드를 제어하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/BackgroundContoller.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 백그라운드를 제어하는 오브젝트입니다.
+/// </summary>
+/// <remarks>
+/// 이 오브젝트는 세팅 씬의 'DAY'와 'NIGHT' 버튼과 함께 동작합니다.
+/// </remarks>
+public class BackgroundContoller : MonoBehaviour
+{
+    /// <summary>
+    /// 세팅 씬의 'DAY' 버튼을 클릭했을 때 수행할 동작입니다.
+    /// </summary>
+    public void OnClickDay()
+    {
+
+    }
+
+    /// <summary>
+    /// 세팅 씬의 'NIGHT' 버튼을 클릭했을 때 수행할 동작입니다.
+    /// </summary>
+    public void OnClickNight()
+    {
+
+    }
+}
