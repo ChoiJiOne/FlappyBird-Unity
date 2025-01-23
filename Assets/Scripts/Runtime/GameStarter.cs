a5d7a7243132c6ee45e783ab28710035bf1f1a51
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-23 14:06:42 +0900

    게임을 시작하는 오브젝트의 스크립트 추가

---
+++ Assets/Scripts/Runtime/GameStarter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 씬을 전환하여 게임을 시작합니다.
+/// </summary>
+/// <remarks>
+/// 이 오브젝트는 선택 씬의 StartButton과 함께 동작합니다.
+/// </remarks>
+public class GameStarter : MonoBehaviour
+{
+    /// <summary>
+    /// START 버튼을 클릭했을 때 동작합니다.
+    /// </summary>
+    public void OnClickStart()
+    {
+        
+    }
+}
