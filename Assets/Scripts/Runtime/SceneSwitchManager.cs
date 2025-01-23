c53c49cd43aabc70b22839cc8e74a9f95ef2f0b1
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-23 09:27:44 +0900

    취소 버튼을 클릭했을 때의 동작 추가

--- Assets/Scripts/Runtime/SceneSwitchManager.cs
+++ Assets/Scripts/Runtime/SceneSwitchManager.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /// <summary>
-/// 시작 씬 내에서 버튼과 함께 씬 전환을 수행하는 매니저입니다.
+/// 버튼과 함께 씬 전환을 수행하는 매니저입니다.
 /// </summary>
 public class SceneSwitchManager : MonoBehaviour
 {
     /// <summary>
-    /// 플레이 버튼을 클릭했을 때 전환할 씬의 이름입니다.
+    /// 시작 씬에서 플레이 버튼을 클릭했을 때 전환할 씬의 이름입니다.
     /// </summary>
     private string _selectSceneName;
 
     /// <summary>
-    /// 세팅 버튼을 클릭했을 때 전환할 씬의 이름입니다.
+    /// 시작 씬에서 세팅 버튼을 클릭했을 때 전환할 씬의 이름입니다.
     /// </summary>
     private string _settingSceneName;
+
+    /// <summary>
+    /// 세팅 씬에서 취소 버튼을 클릭했을 때 전환할 씬의 이름입니다.
+    /// </summary>
+    private string _startSceneName;
 
     /// <summary>
     /// 전환될 씬 이름을 설정합니다.
@@ -25,6 +30,7 @@ public class SceneSwitchManager : MonoBehaviour
     {
         _selectSceneName = "SelectScene";
         _settingSceneName = "SettingScene";
+        _startSceneName = "StartScene";
     }
 
     /// <summary>
@@ -48,5 +54,13 @@ public class SceneSwitchManager : MonoBehaviour
     public void OnClickSetting()
     {
         SceneManager.LoadScene(_settingSceneName);
+    }
+
+    /// <summary>
+    /// 취소 버튼을 클릭했을 때 수행할 동작입니다.
+    /// </summary>
+    public void OnClickCancel()
+    {
+        SceneManager.LoadScene(_startSceneName);
     }
 }
