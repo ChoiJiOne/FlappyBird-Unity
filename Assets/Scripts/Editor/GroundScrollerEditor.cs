2148ee526488b7384eecd4d0804e52dfb0424646
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-24 17:04:44 +0900

    그라운드 스크롤 속도 제어를 위한 에디터 커스텀 스크립트 추가

---
+++ Assets/Scripts/Editor/GroundScrollerEditor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// 그라운드를 스크롤하는 스크롤러의 프로퍼티를 설정합니다.
+/// </summary>
+[CustomEditor(typeof(GroundScroller))]
+[CanEditMultipleObjects]
+public class GroundScrollerEditor : Editor
+{
+
+}
