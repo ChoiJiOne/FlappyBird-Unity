fea1e9484c17fed3773ec459e1982cb53404388b
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-24 14:47:57 +0900

    게임 시작 버튼을 클릭했을 때 수행할 동작 추가

--- Assets/Scripts/Runtime/GameStarter.cs
+++ Assets/Scripts/Runtime/GameStarter.cs
@@ -1,21 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// 게임을 시작합니다.
 /// </summary>
 public class GameStarter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 플레이어가 게임을 플레이하는 씬의 이름입니다.
+    /// </summary>
+    private string _playSceneName;
+
+    /// <summary>
+    /// 플레이어가 게임을 플레이하는 씬의 이름을 설정합니다.
+    /// </summary>
+    private void Awake()
     {
-        
+        _playSceneName = "PlayScene";
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 게임 시작 버튼을 클릭했을 때 수행할 동작입니다.
+    /// </summary>
+    public void OnClickStartButton()
     {
-        
+        SceneManager.LoadScene(_playSceneName);
     }
 }
