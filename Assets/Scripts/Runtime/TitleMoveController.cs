e2c0a6f117272ad0929ecd1f345bca5b7aeb0c33
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-24 10:24:20 +0900

    타이틀 UI의 랙 트랜스폼 초기화

--- Assets/Scripts/Runtime/TitleMoveController.cs
+++ Assets/Scripts/Runtime/TitleMoveController.cs
@@ -8,11 +8,19 @@ using UnityEngine;
 public class TitleMoveController : MonoBehaviour
 {
     /// <summary>
-    /// 
+    /// 타이틀 UI의 랙 트랜스폼입니다.
+    /// </summary>
+    /// <remakrs>
+    /// 타이틀 랙 트랜스폼의 앵커(Anchors)는 Canvas 기준으로 center/middle 입니다.
+    /// </remakrs>
+    private RectTransform _rectTransform;
+
+    /// <summary>
+    /// 랙 트랜스폼을 설정합니다.
     /// </summary>
     void Start()
     {
-        
+        _rectTransform = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
