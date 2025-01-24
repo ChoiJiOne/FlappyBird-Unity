b0c1eafbaa0af17ead027e51fce28c73797c19bf
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-24 10:20:11 +0900

    시작 씬 타이틀의 움직임을 제어하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/TitleMoveController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 시작 씬 타이틀의 움직임을 제어합니다.
+/// </summary>
+public class TitleMoveController : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
