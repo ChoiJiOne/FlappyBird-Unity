987c55615c4708d986041acedb22b47d311a9696
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-24 09:37:30 +0900

    백그라운드의 스프라이트를 제어하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/BgSpriteController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 백그라운드의 스프라이트를 제어합니다.
+/// </summary>
+public class BgSpriteController : MonoBehaviour
+{
+}
