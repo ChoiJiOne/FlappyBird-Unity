90816311eb600f1e40830c2bdd99a777dcb4b74f
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-24 09:40:14 +0900

    멤버 변수의 스프라이트 렌더러 초기화

--- Assets/Scripts/Runtime/BgSpriteController.cs
+++ Assets/Scripts/Runtime/BgSpriteController.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 /// <summary>
 /// 백그라운드의 스프라이트를 제어합니다.
 /// </summary>
+/// <remarks>
+/// 이 스크립트는 씬 내의 백그라운드 오브젝트와 함께 동작합니다.
+/// </remarks>
 public class BgSpriteController : MonoBehaviour
 {
+    /// <summary>
+    /// 배경 스프라이트 렌더링을 수행할 렌더러입니다.
+    /// </summary>
+    private SpriteRenderer _spriteRenderer;
+
+    /// <summary>
+    /// 스프라이트 렌더러를 설정합니다.
+    /// </summary>
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 }
