903e357f8b6c8604022493e9e2e2d64894887ec8
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-24 11:28:32 +0900

    플레이 버튼을 클릭했을 때 선택 씬으로 전환하는 기능 추가

--- Assets/Scripts/Runtime/SceneSwitchTrigger.cs
+++ Assets/Scripts/Runtime/SceneSwitchTrigger.cs
@@ -14,18 +14,32 @@ public class SceneSwitchTrigger : MonoBehaviour
     private string _settingSceneName;
 
     /// <summary>
+    /// 게임 시작 전 선택을 수행하는 씬의 이름입니다.
+    /// </summary>
+    private string _selectSceneName;
+
+    /// <summary>
     /// 전환할 씬의 이름 값을 초기화합니다.
     /// </summary>
     private void Awake()
     {
         _settingSceneName = "SettingScene";
+        _selectSceneName = "SelectScene";
     }
 
     /// <summary>
     /// 세팅 버튼을 클릭했을 때 수행할 동작입니다.
     /// </summary>
-    public void OnClickSetting()
+    public void OnClickSettingButton()
     {
         SceneManager.LoadScene(_settingSceneName);
+    }
+
+    /// <summary>
+    /// 플레이 버튼을 클릭했을 때 수행할 동작합니다.
+    /// </summary>
+    public void OnClickPlayButton()
+    {
+        SceneManager.LoadScene(_selectSceneName);
     }
 }
