ae8bbceb527492baf56d5ed869a4f12505f85ff4
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-24 15:59:14 +0900

    그라운드(바닥)의 스크롤링을 수행하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/GroundScroller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 그라운드(바닥)의 스크롤링을 수행합니다.
+/// </summary>
+public class GroundScroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
