c63d17fdca1d5416b5b98c01af24764f521e0dc9
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-24 11:19:31 +0900

    세팅 버튼 클릭 시 세팅 씬으로 전환하는 기능 추가

--- Assets/Scripts/Runtime/SceneSwitchTrigger.cs
+++ Assets/Scripts/Runtime/SceneSwitchTrigger.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// 시작 씬의 씬 전환을 수행합니다.
+/// </summary>
 public class SceneSwitchTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 게임 설정을 수행하는 씬의 이름입니다.
+    /// </summary>
+    private string _settingSceneName;
+
+    /// <summary>
+    /// 전환할 씬의 이름 값을 초기화합니다.
+    /// </summary>
+    private void Awake()
     {
-        
+        _settingSceneName = "SettingScene";
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 세팅 버튼을 클릭했을 때 수행할 동작입니다.
+    /// </summary>
+    public void OnClickSetting()
     {
-        
+        SceneManager.LoadScene(_settingSceneName);
     }
 }
