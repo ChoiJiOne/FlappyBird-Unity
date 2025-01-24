e7a7cbad7064e9626094ae7f51244b9f600a2bdf
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-24 17:08:15 +0900

    에디터에서 스크롤 속도를 설정할 수 있도록 수정

--- Assets/Scripts/Editor/GroundScrollerEditor.cs
+++ Assets/Scripts/Editor/GroundScrollerEditor.cs
@@ -10,5 +10,26 @@ using UnityEditor;
 [CanEditMultipleObjects]
 public class GroundScrollerEditor : Editor
 {
+    /// <summary>
+    /// 그라운드의 스크롤 속도입니다.
+    /// </summary>
+    private SerializedProperty _scrollSpeed;
 
+    /// <summary>
+    /// 에디터에서 제어할 스크롤 속도를 설정합니다.
+    /// </summary>
+    void OnEnable()
+    {
+        _scrollSpeed = serializedObject.FindProperty("_scrollSpeed");
+    }
+
+    /// <summary>
+    /// https://docs.unity3d.com/kr/2018.4/Manual/editor-CustomEditors.html
+    /// </summary>
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(_scrollSpeed);
+        serializedObject.ApplyModifiedProperties();
+    }
 }
