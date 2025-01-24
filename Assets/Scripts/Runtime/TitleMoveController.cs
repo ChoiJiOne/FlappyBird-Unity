93e40ffb5da5aa5100a5dbc4f5137bd898dee9e8
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-24 10:36:19 +0900

    타이틀이 움직일 때 포물선 운동을 수행하는 기능 추가
    - 움직이는 시간 및 거리는 임시로 하드 코딩

--- Assets/Scripts/Runtime/TitleMoveController.cs
+++ Assets/Scripts/Runtime/TitleMoveController.cs
@@ -23,7 +23,7 @@ public class TitleMoveController : MonoBehaviour
     /// <summary>
     /// 타이틀이 움직이는 거리입니다.
     /// </summary>
-    private float _moveLength;
+    private float _moveLength = 30.0f;
 
     /// <summary>
     /// 타이틀이 움직이는 초 단위 시간 값입니다.
@@ -31,7 +31,7 @@ public class TitleMoveController : MonoBehaviour
     /// <remarks>
     /// 움직이는 시간은 위 방향으로 출발해서 원래 위치로 돌아오는 시간입니다.
     /// </remarks>
-    private float _moveTime;
+    private float _moveTime = 1.0f;
 
     /// <summary>
     /// 타이틀의 누적 시간 값입니다.
@@ -50,9 +50,28 @@ public class TitleMoveController : MonoBehaviour
         _moveBasePosition = _rectTransform.anchoredPosition;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// 타이틀이 포물선 운동을 하도록 업데이트합니다.
+    /// </summary>
+    /// <remarks>
+    /// 포물선 운동의 식은 다음과 같습니다.
+    /// 시간 t에 대하여 움직일 때, a초만큼 최대 h 움직일 때,
+    /// y = (4 * h / a^2) * t * (a - t)
+    /// 아래의 코드에서 constant가 (4 * h / a^2)를 계산한 것입니다.
+    /// </remarks>
     void Update()
     {
-        
+        _currentStepTime += Time.deltaTime;
+        if(_currentStepTime > _moveTime)
+        {
+            _currentStepTime -= _moveTime;
+        }
+
+        float constant = (4.0f * _moveLength) / (_moveTime * _moveTime);
+
+        Vector2 newMovePosition = _moveBasePosition;
+        newMovePosition.y += constant * _currentStepTime * (_moveTime - _currentStepTime);
+
+        _rectTransform.anchoredPosition = newMovePosition;
     }
 }
