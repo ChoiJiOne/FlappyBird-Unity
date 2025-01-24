88da137e6d20ab7cb86d10e26f5100aefcf51db1
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-24 10:39:41 +0900

    타이틀 이동을 제어하는 스크립트의 내부 변수를 에디터에서 제어할 수 있는 스크립트 추가

---
+++ Assets/Scripts/Editor/TitleMoveControllerEditor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 타이틀 움직임을 제어하는 컨트롤러의 프로퍼티를 설정합니다.
+/// </summary>
+public class TitleMoveControllerEditor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
