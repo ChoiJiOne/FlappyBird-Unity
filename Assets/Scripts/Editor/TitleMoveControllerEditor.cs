aefe7915c4cc5973857c9e7c225b2c7f8f9eb0eb
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-24 10:48:29 +0900

    에디터에서 타이틀의 이동 거리를 제어할 수 있는 기능 추가

--- Assets/Scripts/Editor/TitleMoveControllerEditor.cs
+++ Assets/Scripts/Editor/TitleMoveControllerEditor.cs
@@ -1,21 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 /// <summary>
 /// 타이틀 움직임을 제어하는 컨트롤러의 프로퍼티를 설정합니다.
 /// </summary>
-public class TitleMoveControllerEditor : MonoBehaviour
+[CustomEditor(typeof(TitleMoveController))]
+[CanEditMultipleObjects]
+public class TitleMoveControllerEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 에디터에서 제어할 타이틀 이동 거리입니다.
+    /// </summary>
+    /// <remarks>
+    /// https://docs.unity3d.com/6000.0/Documentation/ScriptReference/SerializedProperty.html
+    /// </remarks>
+    private SerializedProperty _moveLength;
+
+    /// <summary>
+    /// 에디터에서 제어할 타이틀 이동 거리를 설정합니다.
+    /// </summary>
+    void OnEnable()
     {
-        
+        _moveLength = serializedObject.FindProperty("_moveLength");
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// https://docs.unity3d.com/kr/2018.4/Manual/editor-CustomEditors.html
+    /// </summary>
+    public override void OnInspectorGUI()
     {
-        
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(_moveLength);
+        serializedObject.ApplyModifiedProperties();
     }
 }
