e6b56703f53f48e127f055dceecb35bf53094550
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-24 12:06:27 +0900

    백그라운드 스프라이트 컨트롤러 오브젝트를 초기화

--- Assets/Scripts/Runtime/BgSwitchController.cs
+++ Assets/Scripts/Runtime/BgSwitchController.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+/// <summary>
+/// 백그라운드 전환을 수행합니다.
+/// </summary>
 public class BgSwitchController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// 백그라운드의 스프라이트를 관리합니다.
+    /// </summary>
+    private BgSpriteController _bgSpriteController;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 백그라운드의 스프라이트 관리 오브젝트를 초기화합니다.
+    /// </summary>
+    private void Awake()
     {
-        
+        GameObject background = GameObject.Find("Background");
+        _bgSpriteController = background.GetComponent<BgSpriteController>();
     }
 }
