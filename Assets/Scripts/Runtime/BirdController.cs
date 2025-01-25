c1b1bb16fd4e6ce7efd75ee80ff7cb2ca385c208
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-26 07:49:29 +0900

    새를 제어하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/BirdController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 새를 제어합니다.
+/// </summary>
+public class BirdController : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
