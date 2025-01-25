00fd2f5daa5f61ad037bd7015ad885304e630496
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-26 07:54:30 +0900

    새의 색상 별 애니메이션 클립을 제어하는 애니메이터 초기화

--- Assets/Scripts/Runtime/BirdController.cs
+++ Assets/Scripts/Runtime/BirdController.cs
@@ -7,9 +7,17 @@ using UnityEngine;
 /// </summary>
 public class BirdController : MonoBehaviour
 {
+    /// <summary>
+    /// 새의 색상 별 애니메이션 클립을 제어합니다.
+    /// </summary>
+    private Animator _animator;
+
+    /// <summary>
+    /// 새의 색상 별 애니메이션 클립을 제어하는 애니메이터를 초기화합니다.
+    /// </summary>
     private void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
