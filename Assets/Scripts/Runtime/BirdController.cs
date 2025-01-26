a99477bf3239aac23705d2cc1d13ec41d311a2bf
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-26 09:58:25 +0900

    새가 점프를 뛸 수 있는지 확인하는 기능 추가

--- Assets/Scripts/Runtime/BirdController.cs
+++ Assets/Scripts/Runtime/BirdController.cs
@@ -73,4 +73,15 @@ public class BirdController : MonoBehaviour
             _isJump = false;
         }
     }
+
+    /// <summary>
+    /// 새가 점프를 뛸 수 있는지 확인합니다.
+    /// </summary>
+    /// <returns>
+    /// 점프를 뛸 수 있다면 true, 그렇지 않으면 false를 반환합니다.
+    /// </returns>
+    private bool CanJump()
+    {
+        return _currentState == BirdState.IDLE || _currentState == BirdState.FALL;
+    }
 }
