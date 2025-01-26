e5b4993d2e7ffe4d54c13ed140662c9780b64744
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-26 09:46:47 +0900

    새의 점프 기능 추가

--- Assets/Scripts/Runtime/BirdController.cs
+++ Assets/Scripts/Runtime/BirdController.cs
@@ -13,15 +13,42 @@ public class BirdController : MonoBehaviour
     private Animator _animator;
 
     /// <summary>
+    /// 새의 움직임 및 충돌 처리를 위한 강체(RigidBody)입니다.
+    /// </summary>
+    private Rigidbody2D _rigidBody;
+
+    /// <summary>
+    /// 새가 점프하는지 확인합니다. 
+    /// </summary>
+    /// <remarks>
+    /// 새가 점프하고있다면 true, 그렇지 않으면 false입니다.
+    /// </remarks>
+    private bool _isJump = false;
+
+    /// <summary>
     /// 새의 색상 별 애니메이션 클립을 제어하는 애니메이터를 초기화합니다.
     /// </summary>
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && !_isJump)
+        {
+            Vector2 velocity = Vector2.zero;
+            velocity.x = _rigidBody.velocity.x;
+            velocity.y = 10.0f;
+            _rigidBody.velocity = velocity;
+
+            _isJump = true;
+        }
+
+        if (_isJump && _rigidBody.velocity.y <= 0.0f)
+        {
+            _isJump = false;
+        }
     }
 }
