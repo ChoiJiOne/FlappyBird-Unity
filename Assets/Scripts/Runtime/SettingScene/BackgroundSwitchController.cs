30dc273cde4b4370e51d9f1e3545c81681e5184e
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-27 15:46:22 +0900

    파일 이름과 클래스 이름이 동일하도록 수정

--- Assets/Scripts/Runtime/SettingScene/BackgroundSwitchController.cs
+++ Assets/Scripts/Runtime/SettingScene/BackgroundSwitchController.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 /// <remarks>
 /// 이 백그라운드 스위치 컨트롤러는 세팅 씬 내부에서만 사용됩니다.
 /// </remarks>
-public class BgSwitchController : MonoBehaviour
+public class BackgroundSwitchController : MonoBehaviour
 {
     /// <summary>
     /// 세팅 씬 내의 백그라운드 오브젝트입니다.
@@ -23,7 +23,7 @@ public class BgSwitchController : MonoBehaviour
     /// <summary>
     /// 백그라운드의 스프라이트를 관리합니다.
     /// </summary>
-    private BackgroundSwitchController _bgSpriteController;
+    private BgSpriteController _bgSpriteController;
 
     /// <summary>
     /// 백그라운드의 스프라이트 관리 오브젝트를 초기화합니다.
@@ -31,7 +31,7 @@ public class BgSwitchController : MonoBehaviour
     private void Awake()
     {
         GameObject background = GameObject.Find("Background");
-        _bgSpriteController = background.GetComponent<BackgroundSwitchController>();
+        _bgSpriteController = background.GetComponent<BgSpriteController>();
     }
 
     /// <summary>
@@ -42,7 +42,7 @@ public class BgSwitchController : MonoBehaviour
     /// </remarks>
     public void OnClickSwitchDayButton()
     {
-        if (BackgroundSwitchController.Day)
+        if (BgSpriteController.Day)
         {
             return;
         }
@@ -58,7 +58,7 @@ public class BgSwitchController : MonoBehaviour
     /// </remarks>
     public void OnClickSwitchNightButton()
     {
-        if (!BackgroundSwitchController.Day)
+        if (!BgSpriteController.Day)
         {
             return;
         }
@@ -74,7 +74,7 @@ public class BgSwitchController : MonoBehaviour
     /// </remarks>
     private void SwitchBackgroundSprite()
     {
-        BackgroundSwitchController.Day = !BackgroundSwitchController.Day;
+        BgSpriteController.Day = !BgSpriteController.Day;
         _bgSpriteController.ApplySprite();
     }
 }
