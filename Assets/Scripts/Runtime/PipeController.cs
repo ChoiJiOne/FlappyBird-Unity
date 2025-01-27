718cc786820177a4a42274cca2a13c6cca308353
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-27 09:13:07 +0900

    파이프의 움직임을 제어하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/PipeController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 파이프의 움직임을 제어합니다.
+/// </summary>
+public class PipeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
