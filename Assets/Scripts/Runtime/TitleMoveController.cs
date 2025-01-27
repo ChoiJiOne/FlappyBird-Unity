36367915a2696cdef2a22390e20d2ed393b9e48e
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-27 22:42:38 +0900

    타이틀이 포물선 운동을 수행하도록 위치를 업데이트

--- Assets/Scripts/Runtime/TitleMoveController.cs
+++ Assets/Scripts/Runtime/TitleMoveController.cs
@@ -52,9 +52,28 @@ public class TitleMoveController : MonoBehaviour
         _moveBasePosition = _rectTransform.anchoredPosition;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// 타이틀이 포물선 운동을 수행하도록 위치를 업데이트합니다.
+    /// </summary>
+    /// <remarks>
+    /// 포물선 운동의 식은 다음과 같습니다.
+    /// 시간 t에 대하여 움직일 때, a초만큼 최대 h 움직일 때,
+    /// y = (4 * h / a^2) * t * (a - t)
+    /// 아래의 코드에서 constant가 (4 * h / a^2)를 계산한 것입니다.
+    /// </remarks>
     private void Update()
     {
-        
+        _currentStepTime += Time.deltaTime;
+        if (_currentStepTime > _moveTime)
+        {
+            _currentStepTime -= _moveTime;
+        }
+
+        float constant = (4.0f * _moveLength) / (_moveTime * _moveTime);
+
+        Vector2 newMovePosition = _moveBasePosition;
+        newMovePosition.y += constant * _currentStepTime * (_moveTime - _currentStepTime);
+
+        _rectTransform.anchoredPosition = newMovePosition;
     }
 }
