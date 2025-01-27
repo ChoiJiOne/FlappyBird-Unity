ec53bb15755f981ce3a248f5d49bb3dd16c0d7cb
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-27 09:34:17 +0900

    파이프의 이동 속도 및 이동 속도에 대한 프로퍼티 추가

--- Assets/Scripts/Runtime/PipeController.cs
+++ Assets/Scripts/Runtime/PipeController.cs
@@ -6,7 +6,16 @@ using UnityEngine;
 /// 파이프의 움직임을 제어합니다.
 /// </summary>
 public class PipeController : MonoBehaviour
-{
+{    
+    /// <summary>
+    /// 파이프의 이동 속도를 설정하는 프로퍼티입니다.
+    /// </summary>
+    public float Speed
+    {
+        get { return _moveSpeed; }
+        set { _moveSpeed = value; }
+    }
+
     /// <summary>
     /// 파이프의 움직임을 설정하는 프로퍼티입니다.
     /// </summary>
@@ -15,17 +24,20 @@ public class PipeController : MonoBehaviour
         get { return _canMove; }
         set { _canMove = value; }
     }
+    
+    /// <summary>
+    /// 파이프의 이동 속도입니다.
+    /// </summary>
+    private float _moveSpeed = 0.0f;
 
     /// <summary>
     /// 파이프의 움직임 여부입니다.
     /// </summary>
     private bool _canMove = false;
 
-    private void Start()
-    {
-    }
-
-    // Update is called once per frame
+    /// <summary>
+    /// 파이프가 움직일 수 있다면 왼쪽에서 오른쪽으로 이동합니다.
+    /// </summary>
     private void Update()
     {
     }
