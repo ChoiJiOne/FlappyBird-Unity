fb748c1e86225fe911d8d7e00299ac36b015d9c8
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-27 13:53:55 +0900

    최초에 파이프 오브젝트를 생성하고 큐에 삽입

--- Assets/Scripts/Runtime/PipeScheduler.cs
+++ Assets/Scripts/Runtime/PipeScheduler.cs
@@ -2,16 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 플레이어가 피해야 할 파이프를 스케줄링합니다.
+/// </summary>
 public class PipeScheduler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 파이프의 Y좌표 최댓값입니다.
+    /// </summary>
+    private const float MAX_Y_POSITION = 3.0f;
+
+    /// <summary>
+    /// 파이프의 Y좌표 최솟값입니다.
+    /// </summary>
+    private const float MIN_Y_POSITION = 0.0f;
+
+    /// <summary>
+    /// 스케쥴링해야 할 파이프의 최대 개수입니다.
+    /// </summary>
+    private const int MAX_PIPE_COUNT = 5;
+
+    /// <summary>
+    /// 파이프 오브젝트의 프리팹입니다.
+    /// </summary>
+    public GameObject _pipePrefab;
+
+    /// <summary>
+    /// 스케쥴링해야 할 파이프 오브젝트입니다.
+    /// </summary>
+    private Queue<GameObject> _pipeObjects;
+
+    /// <summary>
+    /// 스케줄링할 파이프를 생성합니다.
+    /// </summary>
+    private void Awake()
     {
-        
+        _pipeObjects = new Queue<GameObject>();
+        for(int count = 0; count < MAX_PIPE_COUNT; ++count)
+        {
+            Vector3 position = transform.position;
+
+            GameObject pipe = Instantiate(_pipePrefab);
+            pipe.SetActive(false);
+
+            _pipeObjects.Enqueue(pipe);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
