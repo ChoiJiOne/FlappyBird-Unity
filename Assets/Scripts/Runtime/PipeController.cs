d5912abadba90334d68f4ee520d408f42b62ae48
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-28 08:36:49 +0900

    파이프를 움직이는 기능 추가

--- Assets/Scripts/Runtime/PipeController.cs
+++ Assets/Scripts/Runtime/PipeController.cs
@@ -35,7 +35,22 @@ public class PipeController : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// 파이프의 움직임을 수행합니다.
+    /// </summary>
+    /// <remarks>
+    /// 이때, 이동은 오른쪽에서 왼쪽으로 진행되고 움직임이 비활성화 되어 있다면 아무 동작도 수행하지 않습니다.
+    /// </remarks>
     private void Update()
     {
+        // 움직임이 비활성화 되면 아무 동작도 수행하지 않음.
+        if (!_canMove)
+        {
+            return;
+        }
+
+        Vector2 currentPosition = transform.position;
+        currentPosition.x -= Time.deltaTime * _moveSpeed;
+        transform.position = currentPosition;
     }
 }
