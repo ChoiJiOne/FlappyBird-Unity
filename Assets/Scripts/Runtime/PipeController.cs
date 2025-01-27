5c90df3b292facc8abbda783586a92ed3a7fd94e
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-28 08:00:27 +0900

    게임 플레이 씬 내의 파이프 오브젝트를 제어하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/PipeController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 게임 플레이 씬 내의 파이프 오브젝트를 제어합니다.
+/// </summary>
+public class PipeController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
