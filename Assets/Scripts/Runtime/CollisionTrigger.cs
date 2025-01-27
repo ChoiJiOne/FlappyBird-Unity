3c8b8d9487ea33452edb10c5f0541d901f8508c9
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-27 13:04:21 +0900

    오브젝트의 충돌 처리를 감지하여 이벤트를 실행시키는 기능 추가

--- Assets/Scripts/Runtime/CollisionTrigger.cs
+++ Assets/Scripts/Runtime/CollisionTrigger.cs
@@ -8,12 +8,56 @@ using UnityEngine;
 public class CollisionTrigger : MonoBehaviour
 {
     /// <summary>
+    /// 충돌 감지 시 실행할 이벤트를 설정할 때 사용할 프로퍼티입니다.
+    /// </summary>
+    public CollisionTriggerEventHandler OnTriggerEnterEvent
+    {
+        set { _onTriggerEnterEvent = value; }
+    }
+
+    /// <summary>
+    /// 충돌이 최초 발생한 이후에 충돌이 계속 진행되고 있을 때 호출할 이벤트를 설정할 때 사용할 프로퍼티입니다.
+    /// </summary>
+    public CollisionTriggerEventHandler OnTriggerStayEvent
+    {
+        set { _onTriggerStayEvent = value; }
+    }
+
+    /// <summary>
+    /// 충돌이 발생한 이후 충돌에서 벗어났을 때 호출할 이벤트를 설정할 때 사용할 프로퍼티입니다.
+    /// </summary>
+    public CollisionTriggerEventHandler OnTriggerExitEvent
+    {
+        set { _onTriggerExitEvent = value; }
+    }
+
+    /// 충돌 감지 시 실행할 이벤트입니다.
+    /// </summary>
+    /// <param name="collision">해당 오브젝트와 충돌한 또 다른 오브젝트의 충돌체(Collider)입니다.</param>
+    public delegate void CollisionTriggerEventHandler(Collider2D collision);
+
+    /// <summary>
+    /// 충돌이 최초 발생했을 때 호출할 이벤트입니다.
+    /// </summary>
+    private CollisionTriggerEventHandler _onTriggerEnterEvent;
+
+    /// <summary>
+    /// 충돌이 최초 발생한 이후에 충돌이 계속 진행되고 있을 때 호출할 이벤트입니다.
+    /// </summary>
+    private CollisionTriggerEventHandler _onTriggerStayEvent;
+
+    /// <summary>
+    /// 충돌이 발생한 이후 충돌에서 벗어났을 때 호출할 이벤트입니다.
+    /// </summary>
+    private CollisionTriggerEventHandler _onTriggerExitEvent;
+
+    /// <summary>
     /// 충돌이 최초 발생했을 때 호출됩니다.
     /// </summary>
     /// <param name="collision">해당 오브젝트와 충돌한 또 다른 오브젝트의 충돌체(Collider)입니다.</param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        _onTriggerEnterEvent?.Invoke(collision);
     }
 
     /// <summary>
@@ -23,9 +67,9 @@ public class CollisionTrigger : MonoBehaviour
     /// <remarks>
     /// 충돌이 계속 진행되고 있다면 Update를 호출할 때마다 호출됩니다.
     /// </remarks>
-    private void OnTriggerEnterStay(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-
+        _onTriggerStayEvent?.Invoke(collision);
     }
 
     /// <summary>
@@ -34,6 +78,6 @@ public class CollisionTrigger : MonoBehaviour
     /// <param name="collision">해당 오브젝트와 충돌한 또 다른 오브젝트의 충돌체(Collider)입니다.</param>
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        _onTriggerExitEvent?.Invoke(collision);
     }
 }
