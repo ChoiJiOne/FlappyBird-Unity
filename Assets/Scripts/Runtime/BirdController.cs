2028074e7b5bdbb29f2b25fb8aef69d636b7f2e1
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-28 07:46:41 +0900

    게임 플레이 씬 내의 새를 제어하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/BirdController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 게임 플레이 씬 내의 새를 제어합니다.
+/// </summary>
+public class BirdController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
