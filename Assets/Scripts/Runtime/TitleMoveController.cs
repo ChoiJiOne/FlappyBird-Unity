07bf83a3619b450825e84397a584cf5a7fe991a9
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-27 22:37:21 +0900

    타이틀 UI의 랙 트랜스폼 추가 및 참조 값을 설정

--- Assets/Scripts/Runtime/TitleMoveController.cs
+++ Assets/Scripts/Runtime/TitleMoveController.cs
@@ -7,14 +7,24 @@ using UnityEngine;
 /// </summary>
 public class TitleMoveController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 타이틀 UI의 랙 트랜스폼입니다.
+    /// </summary>
+    /// <remarks>
+    /// 타이틀 랙 트랜스폼의 앵커(Anchors)는 Canvas 기준으로 center/middle 입니다.
+    /// </remarks>
+    private RectTransform _rectTransform;
+
+    /// <summary>
+    /// 타이틀 UI의 랙 트랜스폼 참조 값을 설정합니다.
+    /// </summary>
+    private void Awake()
     {
-        
+        _rectTransform = GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
