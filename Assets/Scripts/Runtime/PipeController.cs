17a539e3b4590f4345df296a6668a22c0fd15cde
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-28 08:07:46 +0900

    파이프가 움직일 수 있는지 확인하는 멤버 변수 추가

--- Assets/Scripts/Runtime/PipeController.cs
+++ Assets/Scripts/Runtime/PipeController.cs
@@ -13,6 +13,14 @@ public class PipeController : MonoBehaviour
     [SerializeField]
     private float _moveSpeed;
 
+    /// <summary>
+    /// 파이프가 움직일 수 있는지 확인합니다.
+    /// </summary>
+    /// <remarks>
+    /// 파이프 오브젝트가 움직일 수 있다면 true, 그렇지 않으면 false입니다.
+    /// </remarks>
+    private bool _canMove = true;
+
     private void Start()
     {
         
