0df146afa4debec2775dc5d2fed5eee7bffdbc1c
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-27 23:02:33 +0900

    게임 플레이 씬 내의 그라운드를 제어하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/GroundController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 게임 플레이 씬 내의 그라운드를 제어합니다.
+/// </summary>
+/// <remarks>
+/// 주로 횡 스크롤 속도 및 충돌 처리를 수행합니다.
+/// </remarks>
+public class GroundController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
