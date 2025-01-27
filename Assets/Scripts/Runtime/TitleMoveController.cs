5e367835b71ac73142268738bb4e0f513a161f33
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-27 22:32:32 +0900

    시작 씬 내의 타이틀 UI 이동을 제어하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/TitleMoveController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 시작 씬 내의 타이틀 UI 이동을 제어합니다.
+/// </summary>
+public class TitleMoveController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
