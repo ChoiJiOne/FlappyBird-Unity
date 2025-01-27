65997a97d2b374e943e9da08be9b5716fd65707d
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-27 22:53:37 +0900

    씬 전환을 수행해야 하는 버튼을 클릭했을 때 실행하는 메서드 추가

--- Assets/Scripts/Runtime/SceneSwitchTrigger.cs
+++ Assets/Scripts/Runtime/SceneSwitchTrigger.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// 씬 전환 감지 및 전환을 수행합니다.
 /// </summary>
+/// <remarks>
+/// 이 SceneSwitchTrigger는 UI 버튼과 함께 동작합니다.
+/// </remarks>
 public class SceneSwitchTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// 전환 대상이 되는 씬의 이름입니다.
+    /// </summary>
+    [SerializeField]
+    private string _targetSwitchSceneName;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 씬 전환을 수행해야 하는 버튼을 클릭했을 때 실행합니다.
+    /// </summary>
+    public void OnClickSceneSwitchButton()
     {
-        
+        SceneManager.LoadScene(_targetSwitchSceneName);
     }
 }
