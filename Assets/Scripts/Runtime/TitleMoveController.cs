aee67e01b3b22ad17e8c2adcd5faa52d1861e7a1
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-27 22:38:23 +0900

    타이틀이 움직일 때 기준이 되는 위치 추가 및 설정

--- Assets/Scripts/Runtime/TitleMoveController.cs
+++ Assets/Scripts/Runtime/TitleMoveController.cs
@@ -16,11 +16,17 @@ public class TitleMoveController : MonoBehaviour
     private RectTransform _rectTransform;
 
     /// <summary>
-    /// 타이틀 UI의 랙 트랜스폼 참조 값을 설정합니다.
+    /// 타이틀이 움직일 때 기준이 되는 위치입니다.
+    /// </summary>
+    private Vector2 _moveBasePosition;
+
+    /// <summary>
+    /// 타이틀 UI의 랙 트랜스폼 참조 값을 설정하고 움직일 때 기준 위치를 지정합니다.
     /// </summary>
     private void Awake()
     {
         _rectTransform = GetComponent<RectTransform>();
+        _moveBasePosition = _rectTransform.anchoredPosition;
     }
 
     // Update is called once per frame
