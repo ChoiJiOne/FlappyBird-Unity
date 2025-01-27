23aee882c0de05e199d03a418e14642809625b53
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-27 22:45:23 +0900

    씬 전환 감지 및 전환을 수행하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/SceneSwitchTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 씬 전환 감지 및 전환을 수행합니다.
+/// </summary>
+public class SceneSwitchTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
