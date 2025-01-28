9b090f95629d2b5c7961225b36c312f83a88322f
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-28 09:40:35 +0900

    게임 내의 파이프 오브젝트를 관리하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/PipeManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 게임 내의 파이프 오브젝트를 관리합니다.
+/// </summary>
+public class PipeManager : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
