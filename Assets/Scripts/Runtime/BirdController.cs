f504bfddb1fdd9e6a6b532304cf48eea6b57237a
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-28 15:25:17 +0900

    플레이어가 제어하는 새의 상태를 나타내는 열거형 추가

--- Assets/Scripts/Runtime/BirdController.cs
+++ Assets/Scripts/Runtime/BirdController.cs
@@ -8,6 +8,23 @@ using UnityEngine;
 public class BirdController : MonoBehaviour
 {
     /// <summary>
+    /// 플레이어가 제어하는 새의 상태입니다.
+    /// </summary>
+    /// <remarks>
+    /// Idle: 게임 시작 전 대기 상태입니다.
+    /// Jump: 클릭하여 새가 점프 중인 상태입니다.
+    /// Fall: 점프가 끝나고 떨어지는 상태입니다.
+    /// Dead: 오브젝트와 충돌한 상태입니다.
+    /// </remarks>
+    public enum State
+    {
+        Idle,
+        Jump,
+        Fall,
+        Dead,
+    }
+
+    /// <summary>
     /// 새의 애니메이션을 제어합니다.
     /// </summary>
     private Animator _animator;
