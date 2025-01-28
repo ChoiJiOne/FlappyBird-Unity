9170114fa141451ee822c18ef412f6d0d4003179
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-28 09:01:56 +0900

    게임 플레이 씬 내의 설정 값 및 이벤트를 관리하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 게임 플레이 씬 내의 설정 값 및 이벤트를 관리합니다.
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+}
