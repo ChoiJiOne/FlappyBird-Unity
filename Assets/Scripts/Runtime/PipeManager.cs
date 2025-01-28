ffcd7eb18c0d3813eea18e8a30850a337e9d8239
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-28 09:42:58 +0900

    파이프 매니저에 파이프의 이동 속력을 설정하는 프로퍼티 추가

--- Assets/Scripts/Runtime/PipeManager.cs
+++ Assets/Scripts/Runtime/PipeManager.cs
@@ -7,6 +7,20 @@ using UnityEngine;
 /// </summary>
 public class PipeManager : MonoBehaviour
 {
+    /// <summary>
+    /// 파이프의 이동 속력을 설정하는 프로퍼티입니다.
+    /// </summary>
+    public float MoveSpeed
+    {
+        get { return _moveSpeed; }
+        set { _moveSpeed = value; }
+    }
+
+    /// <summary>
+    /// 파이프의 이동 속도입니다.
+    /// </summary>
+    private float _moveSpeed;
+
     private void Start()
     {
         
