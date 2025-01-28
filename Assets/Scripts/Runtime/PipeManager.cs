3e356098838efffb4a9f6fac3be4da35429539f9
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-28 11:10:36 +0900

    파이프를 활성화하는 시간 추가

--- Assets/Scripts/Runtime/PipeManager.cs
+++ Assets/Scripts/Runtime/PipeManager.cs
@@ -44,6 +44,17 @@ public class PipeManager : MonoBehaviour
     /// </summary>
     private Queue<GameObject> _waitPipeObjects;
 
+    /// <summary>
+    /// 파이프를 활성화하는 시간입니다.
+    /// </summary>
+    [SerializeField]
+    private float _activePipeStepTime;
+
+    /// <summary>
+    /// 파이프 스케쥴러의 누적 시간값입니다.
+    /// </summary>
+    private float _currentStepTime = 0.0f;
+
     private void Awake()
     {
         _waitPipeObjects = new Queue<GameObject>();
@@ -57,6 +68,8 @@ public class PipeManager : MonoBehaviour
             pipeController.MoveSpeed = _moveSpeed;
             pipeController.EndXPosition = _endXPosition;
             pipeController.Movable = false;
+
+            _waitPipeObjects.Enqueue(pipe);
         }
     }
 
