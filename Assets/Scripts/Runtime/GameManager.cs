6647fd4da9edcb78e4ec20fb5c75bad465ebb92e
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-28 15:16:20 +0900

    각 오브젝트를 제어하는 컴포넌트를 멤버 변수로 선언 및 초기화

--- Assets/Scripts/Runtime/GameManager.cs
+++ Assets/Scripts/Runtime/GameManager.cs
@@ -31,20 +31,46 @@ public class GameManager : MonoBehaviour
     private GameObject _ground;
 
     /// <summary>
-    /// 게임 내의 파이프 오브젝트를 관리하는 매니저입니다.
+    /// 그라운드의 움직임을 제어합니다.
+    /// </summary>
+    private GroundController _groundController;
+
+    /// <summary>
+    /// 게임 내의 파이프 오브젝트를 관리하는 매니저 오브젝트입니다.
     /// </summary>
     [SerializeField]
     private GameObject _pipeManager;
+
+    /// <summary>
+    /// 게임 내의 파이프 오브젝트를 관리하는 매니저 입니다.
+    /// </summary>
+    /// <remarks>
+    /// 이 멤버 변수가 실제로 파이프를 제어합니다.
+    /// </remarks>
+    private PipeManager _pipeMgr;
+
+    /// <summary>
+    /// 게임 내의 플레이어가 제어하는 새 오브젝트입니다.
+    /// </summary>
+    [SerializeField]
+    private GameObject _bird;
+
+    /// <summary>
+    /// 게임 내의 새를 제어합니다.
+    /// </summary>
+    private BirdController _birdController;
 
     /// <summary>
     /// 그라운드 및 파이프의 속력 값을 설정합니다.
     /// </summary>
     private void Awake()
     {
-        GroundController groundController = _ground.GetComponent<GroundController>();
-        groundController.MoveSpeed = _moveSpeed;
+        _groundController = _ground.GetComponent<GroundController>();
+        _groundController.MoveSpeed = _moveSpeed;
 
-        PipeManager pipeManager = _pipeManager.GetComponent<PipeManager>();
-        pipeManager.MoveSpeed = _moveSpeed;
+        _pipeMgr = _pipeManager.GetComponent<PipeManager>();
+        _pipeMgr.MoveSpeed = _moveSpeed;
+
+        _birdController = _bird.GetComponent<BirdController>();
     }
 }
