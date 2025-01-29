ce80e1925e3fd9d62e5a2317912cac800b83f61a
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-29 22:44:51 +0900

    UI의 이동을 제어하는 데 필요한 멤버 변수 추가

--- Assets/Scripts/Runtime/UIMoveController.cs
+++ Assets/Scripts/Runtime/UIMoveController.cs
@@ -2,10 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// UI의 움직임을 제어합니다.
+/// </summary>
 public class UIMoveController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// UI의 이동 시간입니다.
+    /// </summary>
+    /// <remarks>
+    /// 시간의 단위는 초 단위입니다.
+    /// </remarks>
+    [SerializeField]
+    private float _moveTime;
+
+    /// <summary>
+    /// UI의 시작 위치입니다.
+    /// </summary>
+    private Vector2 _startPosition;
+
+    /// <summary>
+    /// UI의 도착 위치입니다.
+    /// </summary>
+    [SerializeField]
+    private Vector2 _endPosition;
+
+    /// <summary>
+    /// UI가 이동하는 동안 누적된 시간 값입니다.
+    /// </summary>
+    /// <remarks>
+    /// 이 값은 _moveTime 값을 넘어설 수 없습니다.
+    /// </remarks>
+    private float _moveStepTime = 0.0f;
+
+    /// <summary>
+    /// UI의 위치를 
+    /// </summary>
+    void Awake()
     {
         
     }
