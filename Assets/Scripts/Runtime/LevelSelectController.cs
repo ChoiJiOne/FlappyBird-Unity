2baa87def6f1a35b6f51996caeffb21a84cf0d73
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-01-30 18:05:13 +0900

    레벨 선택을 제어하는 스크립트 추가

---
+++ Assets/Scripts/Runtime/LevelSelectController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 레벨 선택을 제어합니다.
+/// </summary>
+/// <remarks>
+/// 레벨 선택 컨트롤러는 세팅 씬의 LeftSelectButton와 RightSelectButton가 함께 동작합니다.
+/// </remarks>
+public class LevelSelectController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
