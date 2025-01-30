91e999b18c2e6373819c623def1b466dab9d5a4e
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-30 09:25:23 +0900

    스코어를 0부터 획득한 점수까지 증가시키는 스크립트 추가

---
+++ Assets/Scripts/Runtime/ScoreCounter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 스코어를 0부터 획득한 점수까지 증가시킵니다.
+/// </summary>
+public class ScoreCounter : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
