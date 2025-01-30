4f242540ce932a3f25acd3101149adcbc5671dec
CHOIJIONE\choij <choijione02@gmail.com>
2025-01-30 14:39:00 +0900

    배경 스프라이트 렌더링을 수행하는 렌더러 추가

--- Assets/Scripts/Runtime/BackgroundSpriteController.cs
+++ Assets/Scripts/Runtime/BackgroundSpriteController.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 백그라운드 스프라이트를 제어합니다.
+/// </summary>
+/// <remarks>
+/// 이 스크립트는 씬 내의 백그라운드 오브젝트와 함께 동작합니다.
+/// </remarks>
 public class BackgroundSpriteController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /// <summary>
+    /// 배경 스프라이트 렌더링을 수행할 렌더러입니다.
+    /// </summary>
+    private SpriteRenderer _spriteRenderer;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
